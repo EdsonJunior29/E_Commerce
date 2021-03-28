a1d2233f4a72eccaa7c043f5b91ae11a3a769747
EdsonJunior29 <edsonjos61@gmail.com>
2021-03-28 12:14:26 -0300

    Adicionando o DB teste e Utilizando JPA.

--- src/main/java/br/com/ecommerce/ejunior/domain/Categoria.java
+++ src/main/java/br/com/ecommerce/ejunior/domain/Categoria.java
@@ -2,12 +2,19 @@ package br.com.ecommerce.ejunior.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+
+@Entity
 @NoArgsConstructor 
 @AllArgsConstructor 
 @EqualsAndHashCode
@@ -16,7 +23,8 @@ import lombok.Setter;
 public class Categoria implements Serializable{
 	private static final long serialVersionUID = 1L;
 
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
