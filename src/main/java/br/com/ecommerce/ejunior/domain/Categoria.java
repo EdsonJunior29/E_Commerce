ac67f72e91dd503418356b391f26cecf5dc6a2da
EdsonJunior29 <edsonjos61@gmail.com>
2021-04-26 20:52:07 -0300

    Realacionamento Muitos para Muitos

--- src/main/java/br/com/ecommerce/ejunior/domain/Categoria.java
+++ src/main/java/br/com/ecommerce/ejunior/domain/Categoria.java
@@ -1,11 +1,14 @@
 package br.com.ecommerce.ejunior.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
@@ -27,5 +30,8 @@ public class Categoria implements Serializable{
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
+	
+	@ManyToMany(mappedBy = "categorias")
+	private List<Produto> produtos = new ArrayList<>();
 	
 }
--- src/main/java/br/com/ecommerce/ejunior/domain/Produto.java
+++ src/main/java/br/com/ecommerce/ejunior/domain/Produto.java
@@ -1,11 +1,16 @@
 package br.com.ecommerce.ejunior.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
@@ -27,5 +32,11 @@ public class Produto implements Serializable {
 	private Integer Id;
 	private String Nome;
 	private Double Preco;
+	
+	@ManyToMany
+	@JoinTable(name = "PRODUTO_CATEGORIA",
+			joinColumns = @JoinColumn(name = "produto_id"),
+			inverseJoinColumns = @JoinColumn(name = "categoria_id"))
+	private List<Categoria> categorias = new ArrayList<>();
 
 }
