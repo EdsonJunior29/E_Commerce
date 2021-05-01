73952f20fe15cdbc71ec420a1ab33fc5525ed021
EdsonJunior29 <edsonjos61@gmail.com>
2021-05-01 15:23:54 -0300

    Criadas as claases Cliente e Endereco e foram feitos os relacionamentos

---
+++ src/main/java/br/com/ecommerce/ejunior/domain/Endereco.java
@@ -0,0 +1,51 @@
+package br.com.ecommerce.ejunior.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+public class Endereco implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	@Column(name = "Logradouro",nullable = false , length = 150)
+	private String logradouro;
+	@Column(name = "Numero",nullable = false , length = 10)
+	private String numero;
+	@Column(name = "Complemento",nullable = false , length = 200)
+	private String complemento;
+	@Column(name = "Bairro",nullable = false , length = 50)
+	private String bairro;
+	@Column(name = "CEP",nullable = false , length = 15)
+	private String cep;
+
+	
+	@ManyToOne
+	@JoinColumn(name = "cliente_id")
+	private Cliente cliente;
+
+	@ManyToOne
+	@JoinColumn(name = "cidade_id")
+	private Cidade cidade;
+	
+}
